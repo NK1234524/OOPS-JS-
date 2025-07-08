cb900918f9e5cc0ddef99f2505f1afcef8e33bd4
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-08 18:32:25 +0530

    update

---
+++ Prototype.js
@@ -0,0 +1,14 @@
+let arr = [3,6,1];
+
+arr.sayhello=()=>{
+    console.log("Hello");
+}
+// arr.__proto__.pop =(n)=>{
+//     console.log("pop ",n);
+// }
+
+
+let arr2 = [5,32,3];
+arr2.sayhello = ()=>{
+    console.log("Hello");
+}
