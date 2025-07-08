ece1d3cf30ec5b8e8382b5fee07ccd3c9a4f75c4
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-09 00:02:46 +0530

    update

--- class.js
+++ class.js
@@ -4,9 +4,12 @@ class students{
         this.age=age;
     }
     talk(){
-        console.log(`The name of student is ${this.name} & age is ${this.age} `);
+        console.log(`The name of student is ${this.name} & age is ${this.age}. `);
     }
 }
 
 let student1 = new students("Nishant",34);
 student1.talk();
+
+let student2 = new students("Prashant",21);
+student2.talk();
