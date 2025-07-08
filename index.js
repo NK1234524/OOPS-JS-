5a77d123653c32a61e2991c262db97673308d62d
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-08 18:31:59 +0530

    update

---
+++ index.js
@@ -0,0 +1,19 @@
+const student1 = {
+    name:"Nishant Kumar",
+    marks:34,
+    class : "XII",
+    getmarks: function(){
+        return this.marks;
+    },
+};
+
+const student2 = {
+    name : "Prashant kumar",
+    marks : 56,
+    class : "X",
+    setmarks : function(newmarks){
+        return this.newmarks;
+    },
+};
+
+
