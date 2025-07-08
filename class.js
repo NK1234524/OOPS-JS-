00c14e04bc8a4cb070d10269d16ca08e0e4b1b43
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-09 01:31:59 +0530

    update

--- class.js
+++ class.js
@@ -9,8 +9,22 @@ class students{
     }
 }
 
-let student1 = new students("Nishant",34,100);
-student1.talk();
+class student extends students{
+    constructor(name,age,rollno,marks){
+        console.log("Student extends students");
+        super(name,age,marks);
+        this.rollno=rollno;
+    }
+}
 
-let student2 = new students("Prashant",21,101);
-student2.talk();
+class teacher extends students{
+    constructor(name,age,subject){
+        console.log("Teacher extends students");
+        super(name,age);
+        this.subject = subject;
+    }
+}
+
+let student1 = new student("Nishant kumar",34,12,100);
+let teacher1 = new teacher("Sheela",50,"English");
+console.log(student1.age,student1.name,student1.marks,student1.rollno);
