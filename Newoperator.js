5045c62ef619015dcd961d5320348bb62e3cdb9b
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-08 20:29:45 +0530

    update

--- Newoperator.js
+++ Newoperator.js
@@ -3,8 +3,11 @@ function Person(name,age){
     this.age=age;
     console.log(this);
 }
-Person.prototype.talk = ()=>{
+Person.prototype.talk = function(){
     console.log(`The name is ${this.name} & ${this.age}`);
 }
 
 let p1 = new Person("adam",25);
+let p2 = new Person("Madam",35);
+console.log(p1.talk());
+console.log(p2.talk());
